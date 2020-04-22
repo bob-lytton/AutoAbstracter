bfae801ba6e14a6d2ee530c9ea2f1a5b69583059
bob-lytton <39236531+bob-lytton@users.noreply.github.com>
2020-04-22 21:05:43 +0800

    remove useless dir

--- AbstracterSite/AbstracterSite/settings.py
+++ AbstracterSite/AbstracterSite/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.dirname(BASE_DIR)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"frontend/dist/static")
+    os.path.join(BASE_DIR, "frontend/dist/static")
 ]
 
